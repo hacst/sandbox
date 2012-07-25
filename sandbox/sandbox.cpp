dcf5b2222a556a05fd774f286ce39ff8b92c7566
Stefan Hacker <dd0t@users.sourceforge.net>
2012-07-25 10:37:56 +0200

    Add ability to manually set sand ground level (-g,--ground command line switch)

--- sandbox/sandbox.cpp
+++ sandbox/sandbox.cpp
@@ -28,18 +28,21 @@ struct Settings {
    // Command line settings
    int monitor;
    bool fullscreen;
-} settings;
+   int sandPlaneDistanceInMM;
 
-struct DerivedSettings {
 	// Derived from settings
-	RECT monitor;
+	RECT monitorRect;
 
 	int beamerXres;
 	int beamerYres;
 
 	int maxSandDepthInMM;
 	int maxSandHeightInMM;
-} derivedSettings;
+
+	uint16_t boxBottomDistanceInMM;
+
+} settings;
+
 
 #ifdef _WIN32
 
@@ -362,16 +365,24 @@ bool getHomography(VideoCapture &capture, Mat &homography)
 	vector<Point2f> realPoints;
 
 	realPoints.push_back(Point2f(0,0)); // Top left
-	realPoints.push_back(Point2f(derivedSettings.beamerXres,0)); // Top right
-	realPoints.push_back(Point2f(derivedSettings.beamerXres, derivedSettings.beamerYres)); // Bottom right
-	realPoints.push_back(Point2f(0, derivedSettings.beamerYres)); // Bottom left
+	realPoints.push_back(Point2f(settings.beamerXres,0)); // Top right
+	realPoints.push_back(Point2f(settings.beamerXres, settings.beamerYres)); // Bottom right
+	realPoints.push_back(Point2f(0, settings.beamerYres)); // Bottom left
 
 	homography = getPerspectiveTransform(calibPoints, realPoints);
 	return true;
 }
 
-bool getDepthCorrection(VideoCapture &capture, Mat &homography, unsigned short &boxBottomMM)
+bool getDepthCorrection(VideoCapture &capture, Mat &homography, uint16_t &boxBottomDistanceInMM)
 {
+	if (settings.sandPlaneDistanceInMM >= 0) {
+		cout << "Using manual settings for depth correction" << endl;
+		cout << "Sandbox sand level set to: " << settings.sandPlaneDistanceInMM << "mm" << endl;
+		boxBottomDistanceInMM = settings.sandPlaneDistanceInMM + settings.maxSandDepthInMM;
+		cout << "Sandbox box bottom level estimated at: " << boxBottomDistanceInMM << "mm" << endl;
+		return true; // Depth correction already set manually
+	}
+
 	if(!capture.grab())
 		return false;
 
@@ -380,14 +391,14 @@ bool getDepthCorrection(VideoCapture &capture, Mat &homography, unsigned short &
 		return false;
 
 	Mat depthWarpedInMM;
-	warpPerspective(rawDepthInMM, depthWarpedInMM, homography, Size(derivedSettings.beamerXres, derivedSettings.beamerYres));
+	warpPerspective(rawDepthInMM, depthWarpedInMM, homography, Size(settings.beamerXres, settings.beamerYres));
 	
 	uint64_t val = 0;
 	unsigned int num = 0;
 	// Sample a few points to estimate ground level
-	for (int x = 0; x < derivedSettings.beamerXres; x += derivedSettings.beamerXres/16)
+	for (int x = 0; x < settings.beamerXres; x += settings.beamerXres/16)
 	{
-		for (int y = 0; y < derivedSettings.beamerYres; y += derivedSettings.beamerYres/16)
+		for (int y = 0; y < settings.beamerYres; y += settings.beamerYres/16)
 		{
 			val += depthWarpedInMM.at<unsigned short>(Point(x,y));
 			++num;
@@ -395,28 +406,28 @@ bool getDepthCorrection(VideoCapture &capture, Mat &homography, unsigned short &
 	}
 
 	val = val / num;
-	cout << "Sandbox ground level estimated at: " << val << endl;
-	val += derivedSettings.maxSandDepthInMM;
-	cout << "Sandbox deepest stop estimated at: " << val << endl;
+	cout << "Sandbox sand level estimated at: " << val << "mm" << endl;
+	val += settings.maxSandDepthInMM;
+	cout << "Sandbox box bottom level estimated at: " << val << "mm" << endl;
 	
-	boxBottomMM = static_cast<unsigned short>(val);
+	boxBottomDistanceInMM = static_cast<uint16_t>(val);
 
 	//TODO: Perspective correction doesn't help with 3D issues. Should use 4 edge points for reconstructing 3d base plane but that can wait.
 
 	// Create depth correction as level
-	//Mat baseLayer = Mat(derivedSettings.beamerXres, derivedSettings.beamerYres, depthWarpedInMM.type(), val); // Idealized sandbox base
+	//Mat baseLayer = Mat(settings.beamerXres, settings.beamerYres, depthWarpedInMM.type(), val); // Idealized sandbox base
 	//cv::subtract(baseLayer, depthWarpedInMM, depthCorrection);
 	return true;
 }
 
-bool sandboxNormalize(Mat &depthWarped, Mat& depthWarpedNormalized, unsigned short boxBottomMM)
+bool sandboxNormalize(Mat &depthWarped, Mat& depthWarpedNormalized, uint16_t boxBottomDistanceInMM)
 {
 	const size_t rows = depthWarped.rows;
 	const size_t cols = depthWarped.cols;
 	depthWarpedNormalized = Mat(rows, cols, depthWarped.type());
 
-	const uint16_t topOrig = boxBottomMM - derivedSettings.maxSandDepthInMM - derivedSettings.maxSandHeightInMM;
-	const uint16_t range = boxBottomMM - topOrig;
+	const uint16_t topOrig = boxBottomDistanceInMM - settings.maxSandDepthInMM - settings.maxSandHeightInMM;
+	const uint16_t range = boxBottomDistanceInMM - topOrig;
 
 	for (size_t row = 0; row < rows; ++row)
 	{
@@ -424,11 +435,11 @@ bool sandboxNormalize(Mat &depthWarped, Mat& depthWarpedNormalized, unsigned sho
 		{
 			uint16_t val = depthWarped.at<uint16_t>(Point(col, row));
 			// Clip
-			if (val > boxBottomMM) val = boxBottomMM;
+			if (val > boxBottomDistanceInMM) val = boxBottomDistanceInMM;
 			else if (val < topOrig) val = topOrig;
 
 			// Shift and invert
-			val = boxBottomMM - val;
+			val = boxBottomDistanceInMM - val;
 
 			// Scale and save
 			const uint16_t fac = numeric_limits<uint16_t>::max() / range;
@@ -454,12 +465,12 @@ bool sandboxNormalize(Mat &depthWarped, Mat& depthWarpedNormalized, unsigned sho
 
 /*
 // 1024x768 Beamer resolution
-derivedSettings.beamerXres = 1024;
-derivedSettings.beamerYres = 768;
+settings.beamerXres = 1024;
+settings.beamerYres = 768;
 
 // Sandbox constants
-derivedSettings.maxSandDepthInMM = 90;
-derivedSettings.maxSandHeightInMM = 200;
+settings.maxSandDepthInMM = 90;
+settings.maxSandHeightInMM = 200;
 
 */
 
@@ -472,6 +483,7 @@ bool parseSettingsFromCommandline(int argc, char **argv, bool &quit)
 		"{e|enumerate|false|Enumerate monitors and quit}"
 		"{d|depth|90|Maximum sand depth below plane in mm}"
 		"{t|top|200|Maximum sand height above plane in mm}"
+		"{g|ground|-1|Distance of the sand plane to the sensor. (-1 for automatic calibration.)}"
 		"{h|help|false|Print help}";
 
 	CommandLineParser clp(argc, argv, keys);
@@ -501,10 +513,12 @@ bool parseSettingsFromCommandline(int argc, char **argv, bool &quit)
 	settings.fullscreen = clp.get<bool>("f");
 	settings.monitor = clp.get<int>("m");
 
-	derivedSettings.maxSandDepthInMM = clp.get<int>("d");
-	derivedSettings.maxSandHeightInMM = clp.get<int>("t");
+	settings.sandPlaneDistanceInMM = clp.get<int>("g");
+	settings.maxSandDepthInMM = clp.get<int>("d");
+	settings.maxSandHeightInMM = clp.get<int>("t");
 
-	if (!getMonitorRect(settings.monitor, derivedSettings.monitor))
+
+	if (!getMonitorRect(settings.monitor, settings.monitorRect))
 	{
 		cerr << "Failed to get information on monitor " << settings.monitor << endl;
 		cerr << "Use the -e option to enumerate available monitors" << endl;
@@ -512,8 +526,8 @@ bool parseSettingsFromCommandline(int argc, char **argv, bool &quit)
 		return false;
 	}
 
-	derivedSettings.beamerXres = derivedSettings.monitor.right - derivedSettings.monitor.left;
-	derivedSettings.beamerYres = derivedSettings.monitor.bottom - derivedSettings.monitor.top;
+	settings.beamerXres = settings.monitorRect.right - settings.monitorRect.left;
+	settings.beamerYres = settings.monitorRect.bottom - settings.monitorRect.top;
 
 	quit = false;
 	return true;
@@ -536,8 +550,7 @@ int main( int argc, char* argv[] )
 	if(!getHomography(capture, homography))
 		return 1;
 
-	unsigned short boxBottomMM;
-	if(!getDepthCorrection(capture, homography, boxBottomMM))
+	if(!getDepthCorrection(capture, homography, settings.boxBottomDistanceInMM))
 		return 1;
 
 	const std::string BGR_IMAGE = "Bgr Image";
@@ -554,7 +567,7 @@ int main( int argc, char* argv[] )
 
 	if (settings.fullscreen)
 	{
-		if(!fullScreen(derivedSettings.monitor, SAND_NORMALIZED))
+		if(!fullScreen(settings.monitorRect, SAND_NORMALIZED))
 		{
 			cerr << "Failed to fullscreen output window on monitor " << settings.monitor << endl;
 		}
@@ -591,16 +604,16 @@ int main( int argc, char* argv[] )
 		}
 
 		Mat bgrWarped;		
-		warpPerspective(bgrImage, bgrWarped, homography, Size(derivedSettings.beamerXres, derivedSettings.beamerYres));
+		warpPerspective(bgrImage, bgrWarped, homography, Size(settings.beamerXres, settings.beamerYres));
 
 		Mat depthWarped;
-		warpPerspective(depthMap, depthWarped, homography, Size(derivedSettings.beamerXres, derivedSettings.beamerYres));
+		warpPerspective(depthMap, depthWarped, homography, Size(settings.beamerXres, settings.beamerYres));
 
 		Mat depthWarpedNormalized;
-		sandboxNormalize(depthWarped, depthWarpedNormalized, boxBottomMM);
+		sandboxNormalize(depthWarped, depthWarpedNormalized, settings.boxBottomDistanceInMM);
 
 		imshow(SAND_NORMALIZED, depthWarpedNormalized);
-		//unsigned short centerval = depthWarped.at<unsigned short>(Point(derivedSettings.beamerXres/2, derivedSettings.beamerYres/2));
+		//unsigned short centerval = depthWarped.at<unsigned short>(Point(settings.beamerXres/2, settings.beamerYres/2));
 		//cout << centerval << endl;
 
 		imshow(BGR_WARPED, bgrWarped);
